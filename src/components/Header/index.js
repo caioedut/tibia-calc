3ecc1d11e5822e912799cb6b5060bb87c62c639d
Caio Teixeira <caio.teixeira@unyleya.com.br>
2020-07-28 22:35:52 -0300

    Back button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {AppBar, Button, Toolbar, Typography} from '@material-ui/core';
+import {Link, useLocation} from 'react-router-dom';
+import {AppBar, Button, Toolbar, Typography, IconButton} from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 function Header({title}) {
     const menus = [
@@ -10,14 +11,31 @@ function Header({title}) {
         },
     ];
 
+    const route = useLocation().pathname;
+
     return (
         <AppBar position="static" color="primary">
             <Toolbar>
+                {route !== '/' && (
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        component={Link}
+                        to={`${process.env.PUBLIC_URL}/`}
+                    >
+                        <ArrowBackIcon/>
+                    </IconButton>
+                )}
                 <Typography variant="h6" style={{flexGrow: 1}}>
                     {title}
                 </Typography>
                 {menus.map((menu) => (
-                    <Button key={menu.url} component={Link} to={`${process.env.PUBLIC_URL}${menu.url}`} variant="text">
+                    <Button
+                        key={menu.url}
+                        variant="text"
+                        component={Link}
+                        to={`${process.env.PUBLIC_URL}${menu.url}`}
+                    >
                         <span style={{color: '#ffffff'}}>{menu.title}</span>
                     </Button>
                 ))}
