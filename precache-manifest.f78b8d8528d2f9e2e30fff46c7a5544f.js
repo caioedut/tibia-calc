18e442ef7b4d6435d2e94d08ec788a63b02fd96c
Caio Teixeira <caio.teixeira@unyleya.com.br>
2020-07-28 22:09:20 -0300

    Updates

---
+++ precache-manifest.f78b8d8528d2f9e2e30fff46c7a5544f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ddcd51633b0101fe85ba1bd698f41b",
+    "url": "/tibia-calc/index.html"
+  },
+  {
+    "revision": "dbb8109c15408bf16eae",
+    "url": "/tibia-calc/static/css/main.06e35e80.chunk.css"
+  },
+  {
+    "revision": "8fbcd3e07f041084243e",
+    "url": "/tibia-calc/static/js/2.62ccd35e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tibia-calc/static/js/2.62ccd35e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb8109c15408bf16eae",
+    "url": "/tibia-calc/static/js/main.eb2f8225.chunk.js"
+  },
+  {
+    "revision": "45c2808d1b36d52e780b",
+    "url": "/tibia-calc/static/js/runtime-main.ba91fda7.js"
+  }
+]);
